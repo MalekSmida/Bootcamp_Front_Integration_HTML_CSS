db16a4b54233307efac1357bdb59bacb94327644
Malik <msmida96@gmail.com>
2020-02-20 12:11:32 +0100

    #Add: Classnames and Modules CSS

--- src/challenge/signIn/index.js
+++ src/challenge/signIn/index.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import "./index.css";
+
+//Adding classnames to concatenate string
+import cx from "classnames";
+
+//Adding a CSS Modules Stylesheet
+import classes from "./index.module.css";
 
 import facebook from "../../icons/Facebook.png";
 import google from "../../icons/Google.png";
@@ -9,45 +14,53 @@ class signin extends Component {
   state = {};
 
   render() {
+    console.log("Classes: ", classes);
     return (
-      <div className="App">
-        <div className="signin">
+      <div className={classes.App}>
+        <div className={classes.signin}>
           <p>Sign in</p>
           <p>
             To keep connected with us please sign in with your personal info
           </p>
         </div>
-        <div className="grid-container host">
-          <button className="grid-item grid-item-3">
+        <div className={cx(classes.grid_container, { [classes.host]: true })}>
+          <button className={cx(classes.grid_item, classes.grid_item_3)}>
             <img src={google} alt={"google"} />
             Sign in with Google
           </button>
-          <button className="grid-item grid-item-3">
+          <button className={cx(classes.grid_item, classes.grid_item_3)}>
             <img src={facebook} alt={"facebook"} />
             Sign in with facebook
           </button>
-          <button className="grid-item grid-item-3">
+          <button className={cx(classes.grid_item, classes.grid_item_3)}>
             <img src={linkedin} alt={"linkedin"} />
             Sign in with linkedin
           </button>
         </div>
-        <div className="grid-container data">
-          <p className="grid-item grid-item-12">E-mail Adress</p>
+        <div className={cx(classes.grid_container, classes.data)}>
+          <p className={cx(classes.grid_item, classes.grid_item_12)}>
+            E-mail Adress
+          </p>
           <input
-            className="grid-item grid-item-8"
+            className={cx(classes.grid_item, classes.grid_item_8)}
             placeholder="Enter Your email"
           ></input>
-          <p className="grid-item grid-item-6">Password</p>
-          <p className="grid-item grid-item-6">Forget password ?</p>
+          <p className={cx(classes.grid_item, classes.grid_item_6)}>Password</p>
+          <p className={cx(classes.grid_item, classes.grid_item_6)}>
+            Forget password ?
+          </p>
           <input
-            className="grid-item grid-item-8"
+            className={cx(classes.grid_item, classes.grid_item_8)}
             placeholder="Enter Your password"
           ></input>
         </div>
-        <div className="grid-container send">
-          <button className="grid-item grid-item-3">Sign in</button>
-          <p className="grid-item grid-item-12">
-            You don't have an account, <span className="link">Sign up</span>
+        <div className={cx(classes.grid_container, classes.send)}>
+          <button className={cx(classes.grid_item, classes.grid_item_3)}>
+            Sign in
+          </button>
+          <p className={cx(classes.grid_item, classes.grid_item_12)}>
+            You don't have an account,{" "}
+            <span className={classes.link}>Sign up</span>
           </p>
         </div>
       </div>
