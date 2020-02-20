57501f669c3437d4ea906ddc877b0f02e4b84bc1
Malik <msmida96@gmail.com>
2020-02-20 16:14:30 +0100

    #Add: input Filter

--- src/challenge/list/index.js
+++ src/challenge/list/index.js
@@ -5,6 +5,7 @@ import smile from "../../icons/smile.jpg";
 
 class list extends Component {
   state = {
+    input: "",
     participants: [
       {
         name: "Olga Butler",
@@ -38,44 +39,62 @@ class list extends Component {
       }
     ]
   };
+
+  onChangeHandler(e) {
+    const input = e.target.value;
+    this.setState({
+      input
+    });
+  }
+
   render() {
+
     return (
       <div className="App3">
         <div className="search3">
           <i class="icon glyphicon glyphicon-search"></i>
-          <input placeholder="Search for participants by name"></input>
+          <input
+            value={this.state.input}
+            type="text"
+            onChange={e => this.onChangeHandler(e)}
+            placeholder="Search for participants by name"
+          ></input>
         </div>
         <div className="grid-container head3">
           <p className="grid-item grid-item-5">98 PARTICIPANT</p>
           <p className="grid-item grid-item-5">STATUS</p>
         </div>
         <div className="tab">
-          {this.state.participants.map(part => (
-            <div className="grid-container list3">
-              <div className="grid-item grid-item-1 info3">
-                <img src={smile} alt={"smile"} />
+          {this.state.participants
+            .filter(
+              part => part.name.toLowerCase().includes(this.state.input.toLowerCase()) || part.post.toLowerCase().includes(this.state.input.toLowerCase())
+            )
+            .map(part => (
+              <div className="grid-container list3">
+                <div className="grid-item grid-item-1 info3">
+                  <img src={smile} alt={"smile"} />
+                </div>
+                <div className="grid-item grid-item-4 info3">
+                  <div>{part.name}</div>
+                  <div>{part.post}</div>
+                </div>
+                <div
+                  className={
+                    part.status === "Arrived"
+                      ? "grid-item grid-item-5 arrived"
+                      : "grid-item grid-item-5 notArrived"
+                  }
+                >
+                  {part.status}
+                </div>
+                <div className="grid-item grid-item-2 icons3">
+                  {part.print && <i class="icon glyphicon glyphicon-print"></i>}
+                  <i class="icon glyphicon glyphicon-envelope"></i>
+                  <i class="icon glyphicon glyphicon-user"></i>
+                  <i class="icon glyphicon glyphicon-home"></i>
+                </div>
               </div>
-              <div className="grid-item grid-item-4 info3">
-                <div>{part.name}</div>
-                <div>{part.post}</div>
-              </div>
-              <div
-                className={
-                  part.status === "Arrived"
-                    ? "grid-item grid-item-5 arrived"
-                    : "grid-item grid-item-5 notArrived"
-                }
-              >
-                {part.status}
-              </div>
-              <div className="grid-item grid-item-2 icons3">
-                {part.print && <i class="icon glyphicon glyphicon-print"></i>}
-                <i class="icon glyphicon glyphicon-envelope"></i>
-                <i class="icon glyphicon glyphicon-user"></i>
-                <i class="icon glyphicon glyphicon-home"></i>
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     );
